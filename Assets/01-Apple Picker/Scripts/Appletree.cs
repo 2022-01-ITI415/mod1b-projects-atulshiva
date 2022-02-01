c55c107002adf70acf9cf31994931ebd8a461c4c
atulshiva <98130986+atulshiva@users.noreply.github.com>
2022-01-31 23:54:08 -0500

    trying to fix code

---
+++ Assets/01-Apple Picker/Scripts/Appletree.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appletree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float LeftAndRightEdge = 10f;
+    public float chanceToChangeDirection = 0.02f;
+    public float secondsBetweenAppleDrop = 1f;
+
+    void Start()
+        
+    {
+        
+    }
+
+    void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing direction
+        if (pos.x < -LeftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > LeftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+        void FixedUpdate() 
+    { 
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; // Change direction   
+        }
+
+    }
+}
