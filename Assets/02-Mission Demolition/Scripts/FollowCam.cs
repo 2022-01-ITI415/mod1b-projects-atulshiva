50d1bc20d2cebf46fca591938e5fee964bd8e74f
atulshiva <98130986+atulshiva@users.noreply.github.com>
2022-02-09 19:25:12 -0500

    added cloudsphere prefab to cloud

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using System.Collections;
 public class FollowCam : MonoBehaviour
 {
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
     static public GameObject POI; // The static point ofinterest // a
     [Header("Set Dynamically")]
     public float camZ; // The desired Z pos of thecamera
+
     void Awake()
     {
         camZ = this.transform.position.z;
@@ -15,9 +18,14 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return; // return if there is nopoi // b
                                  // Get the position of the poi
         Vector3 destination = POI.transform.position;
+       
+        destination = Vector3.Lerp(transform.position, destination,
+easing);
+
         // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to the destination
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
